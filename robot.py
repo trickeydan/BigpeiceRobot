626f0184f353d8305088b5b27ed189a4af348fd9
Dan Trickey <dan@trickey.xyz>
2016-08-03 15:14:24 +0100

    Added check_location and get_wall_markers

--- robot.py
+++ robot.py
@@ -1,5 +1,5 @@
 from sr import *
-import time
+import time, math
 
 
 
@@ -69,10 +69,40 @@ class Vision(object):
         self.markers = self.R.see()
 
     def get_wall_markers(self):
-        
+        wall_markers = []
+        for marker in self.markers:
+            if marker.info.marker_type == MARKER_ARENA:
+                wall_markers.append(marker)
+                
+        return wall_markers
 
-    def count(self):
-        return len(self.markers)
+
+    def check_location(self):
+        self.R.eyes.update()
+        wall_markers = self.get_wall_markers()
+        marker_positions = [[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],\
+                            [1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],\
+                            [8,1],[8,2],[8,3],[8,4],[8,5],[8,6],[8,7],
+                            [1,8],[2,8],[3,8],[4,8],[5,8],[6,8],[7,8],]
+        total_cartesian = [0,0]
+        for marker in wall_markers:
+            horizontal_distance = math.cos(math.radians(marker.centre.polar.rot_x))*marker.dist
+            if marker.info.offset in range(8):
+                angle = marker.orientation.rot_z + 0
+            elif marker.info.offset in range(8,15):
+                angle = marker.orientation.rot_z + 90
+            elif marker.info.offset in range(15,22):
+                angle = marker.orientation.rot_z + 180
+            elif marker.info.offset in range(22,28):
+                angle = marker.orientation.rot_z + 270
+
+            total_cartesian[0] += marker_positions[marker.info.offset][0]+\
+                                  (math.sin(math.radians(angle))*horizontal_distance
+            total_cartesian[1] += marker_positions[marker.info.offset][1]+\
+                                  (math.cos(math.radians(angle))*horizontal_distance
+        number_of_visible_wall_markers = len(wall_markers)
+
+        return total_cartesian[0]/number_of_visible_wall_markers, total_cartesian[1]/number_of_visible_wall_markers
 
 
 class BaseIO(object):
