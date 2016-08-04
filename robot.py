8a5e9581fa7b31a44cdea765fc3db96051ac757b
Dan Trickey <dan@trickey.xyz>
2016-08-04 17:01:13 +0100

    Turn comnstant adjust

--- robot.py
+++ robot.py
@@ -23,7 +23,7 @@ class Wheels(object):
         self.right = right
 
     def _forward(self,secs,power):
-        self.R.motors[self.left].target = power * 1.13
+        self.R.motors[self.left].target = power * 1.15
         self.R.motors[self.right].target = power
         sleep(secs)
         self.R.motors[self.left].target = 0
@@ -48,7 +48,7 @@ class Motion(Wheels):
 
     driveConstant = 1.8
     drivePower = 25
-    turnConstant = 0.2
+    turnConstant = 0.17
 
     def forward(self,metres):
         log("Forward " + str(metres) + " metres")
